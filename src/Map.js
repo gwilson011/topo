0db34b1346e26cc58a30a8c3623d918a472ecff1
Grace Wilson <gwilson011@gmail.com>
2025-05-07 10:18:06 -0700

    changed api variable name to match git action variable name

--- src/Map.js
+++ src/Map.js
@@ -11,7 +11,7 @@ export const Map = ({ setLoading, setRouteInfo, ...props }) => {
     //const [map, setMap] = useState(null);
     const [elevation, setElevation] = useState();
 
-    const apiKey = process.env.REACT_APP_GOOGLE_API;
+    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
     const mapContainerStyle = {
         width: "50%",
